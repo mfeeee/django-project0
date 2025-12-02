c8c8c32483f8c5067fa9679d57d951821b6949e9
mafe <mfernandarocosta@gmail.com>
2025-12-02 19:36:22 -0300

    :bug: fix: correct rendering of signup view to include status parameter

--- users/views.py
+++ users/views.py
@@ -4,10 +4,11 @@ from django.http.response import Http404
 
 # Create your views here.
 def signup(request):
-    return render(request, 'signup.html')
+    status = request.GET.get('status')
+    return render(request, 'signup.html', {'status': status})
 
 def login(request):
-    return ender(request, 'login.html')
+    return render(request, 'login.html')
 
 def validate_signup(request):
     name = request.POST.get('name')
