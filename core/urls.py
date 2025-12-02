1a10d38b96e2b683ee7f466619225ef73b07bf89
mafe <mfernandarocosta@gmail.com>
2025-12-02 18:03:32 -0300

    :sparkles: feat: add validate_signup URL to urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
+    path('validate_signup/', views.validate_signup, name = 'validate_signup'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
