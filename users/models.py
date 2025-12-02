f14e8285a0d0637b0073207cefa0f8703353b68a
mafe <mfernandarocosta@gmail.com>
2025-12-02 16:51:36 -0300

    :sparkles: feat: add User model with name, email, and password

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.name
