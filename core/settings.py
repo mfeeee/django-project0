dd961f620568f7c6a70a0e179bd62049af27411b
mafe <mfernandarocosta@gmail.com>
2025-12-02 18:00:13 -0300

    :boom: fix: update database settings to use MySQL instead of SQLite

--- core/settings.py
+++ core/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'base_de_dados',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '3308', 
     }
 }
 
