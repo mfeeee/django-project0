de76fbabbfc851dc23c0d04849fed8281592ccbb
mafe <mfernandarocosta@gmail.com>
2025-12-02 18:01:46 -0300

    :sparkles: feat: register User model in admin with custom display settings

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import User
+from django.contrib.admin.decorators import display
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'password')
+    search_fields = ('name', 'email')
+    readonly_fields = ('password',)
+
