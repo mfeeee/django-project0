d783a7794c6c57517230f05697377aff229faabe
mafe <mfernandarocosta@gmail.com>
2025-12-02 14:52:18 -0300

    :sparkles: feat: implement signup and login view functions

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import Http404
 
 # Create your views here.
+def signup(request):
+    return HttpResponse('signup')
+
+def login(request):
+    return HttpResponse('login')
