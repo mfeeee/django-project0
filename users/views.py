e7bb9c5f583e87b1c74430cb98dc9169b4279cd5
mafe <mfernandarocosta@gmail.com>
2025-12-02 19:36:06 -0300

    :sparkles: feat: add validate_signup view to check fields eligibility

--- users/views.py
+++ users/views.py
@@ -8,3 +8,30 @@ def signup(request):
 
 def login(request):
     return ender(request, 'login.html')
+
+def validate_signup(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    user = User.objects.filter(email = email)
+
+    if len(user) > 0:
+        return redirect('/auth/signup/?status=1')
+    
+    if len(name.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/auth/signup/?status=2')
+    
+    if len(password) < 8:
+        return redirect('/auth/signup/?status=3')
+    
+    try:
+        password = hashlib.sha256(password.encode()).hexdigest()
+        user = User(name = name,
+                       email = email,
+                       password = password,
+                       )
+        user.save()
+        return redirect('/auth/signup/?status=0')
+    except:
+        return redirect('/auth/signup/?status=4')
