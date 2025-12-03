535a17a7b8ec4eb01d2d370eb68ee11befbe68d6
mafe <mfernandarocosta@gmail.com>
2025-12-03 10:45:54 -0300

    :sparkles: feat: implement home and course view functions

---
+++ courses/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    if request.session.get('user'):
+        courses = Courses.objects.all()
+        request_user = request.session.get('user')
+        return render(request, 'home.html', {'courses': courses, 'request_user': request_user})
+    else:
+        return redirect('/auth/login/?status=2')
+
+def course(request, id):
+    if request.session.get('user'):
+        classes = Classes.objects.filter(course = id)
+        request_user = request.session.get('user')
+        return render(request, 'course.html', {'classes': classes, 'request_user': request_user})
+    else:
+        return redirect('/auth/login/?status=2')
