f0227c25ce929a6feb79c6e3b68b11a822000502
mafe <mfernandarocosta@gmail.com>
2025-12-03 13:13:38 -0300

    :sparkles: feat: implement comments functionality for classes

--- courses/views.py
+++ courses/views.py
@@ -24,3 +24,20 @@ def classes(request, id):
         return render(request, 'classes.html', {'classes': classes})
     else:
         return redirect('/auth/login/?status=2')
+
+def comments(request):
+    user_id = int(request.POST.get('user_id'))
+    comment = request.POST.get('comment')
+    classes_id = int(request.POST.get('classes_id'))
+
+    comment_instance = comments(user_id = user_id,
+                                comment = comment,
+                                classes_id = classes_id)
+    comment_instance.save()
+
+    comments = comments.objects.filter(classes = classes_id).order_by('-data')
+    only_names = [i.user.name for i in comments]
+    only_comments = [i.comment for i in comments]
+    comments = list(zip(only_names, only_comments))
+
+    return HttpResponse(json.dumps({'status': '1', 'comments': comments }))
