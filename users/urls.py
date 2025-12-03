296e7fa7b70911498b8be89e398a1b538381d6c8
mafe <mfernandarocosta@gmail.com>
2025-12-03 09:45:57 -0300

    :sparkles: feat: add validate_login URL

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('validate_signup/', views.validate_signup, name = 'validate_signup'),
-
+    path('validate_login/', views.validate_login, name = 'validate_login'),
 ]
