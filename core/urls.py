a26077c52fd2e685e694a27e6f84f9a602b3c4cc
mafe <mfernandarocosta@gmail.com>
2025-12-03 10:44:11 -0300

    :sparkles: feat: add home route to include courses URLs

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
+    path('home/', include('courses.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
