efd799efa9297212fad3bf74f949b5d2d52ed3dd
mafe <mfernandarocosta@gmail.com>
2025-12-03 11:43:07 -0300

    :sparkles: feat: add URL pattern for classes view

--- courses/urls.py
+++ courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('course/<int:id>', views.course, name = 'course'),
+    path('classes/<int:id>', views.classes, name = 'classes'),
 ]
